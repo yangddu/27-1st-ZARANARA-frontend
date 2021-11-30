3b3015d55345293ca43f0799e9c2083a94e67b51
yangddu <wndun21@naver.com>
2021-11-30 14:16:53 +0900

    Add: nav 레이아웃

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,6 +1,36 @@
 import React from 'react';
 import './Nav.scss';
 
-const Nav = () => <h1>Nav</h1>;
+const Nav = () => (
+  <header className="header">
+    <div className="navLeftContainer">
+      <img
+        className="menuBar"
+        src="/images/nav/hamburgerMenu.png"
+        alt="hamburgerMenuBar"
+      />
+      <h1>ZARANARA</h1>
+    </div>
+    <div className="navCenterContainer">
+      <div className="searchLink">
+        <a href="javascript:;">
+          검색
+          <span className="line"></span>
+        </a>
+      </div>
+    </div>
+    <div className="navRightContainer">
+      <div className="loginContainer">
+        <div className="loginLink">
+          <img src="/images/nav/user.png" alt="user" />
+          로그인
+        </div>
+        <div className="cartLink">
+          <img src="/images/nav/user.png" alt="user" />0
+        </div>
+      </div>
+    </div>
+  </header>
+);
 
 export default Nav;
