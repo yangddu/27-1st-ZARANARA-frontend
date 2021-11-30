1f80d6f0e13320e4c88cb29373811c07cf9f64ee
yangddu <wndun21@naver.com>
2021-11-30 20:10:03 +0900

    Modify: footer 상수 데이터로 변경

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,41 +1,33 @@
 import React from 'react';
-import './Footer.scss';
+import { asideData, FooterImg } from './FooterData';
 
+import './Footer.scss';
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footerInner">
-        <div className="noticeArea">
-          <div className="noticeTitle">공지사항</div>
-        </div>
         <div className="asideWrap">
           <div className="asideArea">
-            <div className="corpBox">
-              <h3 className="corpTitle">Developers</h3>
-              <a href="#">Team ZARANARA</a>
-            </div>
-            <div className="telBox">
-              <h3 className="telTitle">Tel</h3>
-              <a href="#">연락처</a>
-              <a href="#">GUEST PURCHASE</a>
-              <a href="#">매장찾기</a>
-              <a href="#">010-2727-0505</a>
-            </div>
+            {asideData.map(el => {
+              return (
+                <div className="asideBox" key={el.id}>
+                  <h3 className="asideTitle">{el.title}</h3>
+                  {el.content &&
+                    el.content.map(content => (
+                      <a className="asideContent" href="#">
+                        {content}
+                      </a>
+                    ))}
+                </div>
+              );
+            })}
           </div>
+
           <div className="snsArea">
             <div className="snsBox">
-              <img
-                src="https://cdn-icons-png.flaticon.com/512/20/20837.png"
-                alt="facebook"
-              />
-              <img
-                src="https://cdn-icons.flaticon.com/png/512/717/premium/717392.png?token=exp=1638255524~hmac=b3666a81a2e3a185d78f4dbf3cc085b2"
-                alt="instagram"
-              />
-              <img
-                src="https://cdn-icons-png.flaticon.com/512/2111/2111496.png"
-                alt="kakaoTalk"
-              />
+              {FooterImg.map(el => (
+                <img key={el.id} src={el.src} alt={el.alt} />
+              ))}
             </div>
           </div>
         </div>
---
+++ src/components/Footer/FooterData.js
@@ -0,0 +1,25 @@
+export const asideData = [
+  { id: 1, title: '공지사항' },
+  { id: 2, title: 'Developers', content: ['Team ZARANARA'] },
+  {
+    id: 3,
+    title: 'Tel',
+    content: ['연락처', 'GUEST PURCHASE', '매장찾기', '010-2727-0505'],
+  },
+  // {
+  //   id: 4,
+  //   title: 'sns',
+  //   src: [
+  //     'https://cdn-icons-png.flaticon.com/512/20/20837.png',
+  //     'https://cdn-icons.flaticon.com/png/512/717/premium/717392.png?token=exp=1638255524~hmac=b3666a81a2e3a185d78f4dbf3cc085b2',
+  //     'https://cdn-icons-png.flaticon.com/512/2111/2111496.png',
+  //   ],
+  //   alt: 'img',
+  // },
+];
+
+export const FooterImg = [
+  { id: 1, src: 'https://cdn-icons-png.flaticon.com/512/20/20837.png' },
+  { id: 2, src: 'https://cdn-icons-png.flaticon.com/512/87/87390.png' },
+  { id: 3, src: 'https://cdn-icons-png.flaticon.com/512/2111/2111496.png' },
+];
