455f66b4f8d605bd2f0287f6f6d22c69cd3d45f2
yangddu <wndun21@naver.com>
2021-12-01 13:00:36 +0900

    WIP

--- src/components/Footer/FooterData.js
+++ src/components/Footer/FooterData.js
@@ -6,16 +6,6 @@ export const asideData = [
     title: 'Tel',
     content: ['연락처', 'GUEST PURCHASE', '매장찾기', '010-2727-0505'],
   },
-  // {
-  //   id: 4,
-  //   title: 'sns',
-  //   src: [
-  //     'https://cdn-icons-png.flaticon.com/512/20/20837.png',
-  //     'https://cdn-icons.flaticon.com/png/512/717/premium/717392.png?token=exp=1638255524~hmac=b3666a81a2e3a185d78f4dbf3cc085b2',
-  //     'https://cdn-icons-png.flaticon.com/512/2111/2111496.png',
-  //   ],
-  //   alt: 'img',
-  // },
 ];
 
 export const FooterImg = [
