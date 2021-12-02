b5081c59f466a96c4a77d7969d8952cc5fc28878
yangddu <wndun21@naver.com>
2021-12-02 19:10:16 +0900

    Modify: key값 입력 및 레이아웃 수정

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,28 +5,26 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footerInner">
-        <div className="asideWrap">
-          <div className="asideArea">
-            {ASIDE_DATA.map(el => {
-              return (
-                <div className="asideBox" key={el.id}>
-                  <h3 className="asideTitle">{el.title}</h3>
-                  {el.content?.map(content => (
-                    <a className="asideContent" href="#">
-                      {content}
-                    </a>
-                  ))}
-                </div>
-              );
-            })}
-          </div>
+        <div className="asideArea">
+          {ASIDE_DATA.map(el => {
+            return (
+              <div className="asideBox" key={el.id}>
+                <h3 className="asideTitle">{el.title}</h3>
+                {el.content?.map((content, idx) => (
+                  <a key={idx} className="asideContent" href="#">
+                    {content}
+                  </a>
+                ))}
+              </div>
+            );
+          })}
+        </div>
 
-          <div className="snsArea">
-            <div className="snsBox">
-              {FOOTER_IMG.map(el => (
-                <img key={el.id} src={el.src} alt={el.alt} />
-              ))}
-            </div>
+        <div className="snsArea">
+          <div className="snsBox">
+            {FOOTER_IMG.map(el => (
+              <img key={el.id} src={el.src} alt={el.alt} />
+            ))}
           </div>
         </div>
       </div>
