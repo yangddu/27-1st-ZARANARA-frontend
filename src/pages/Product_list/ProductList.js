b9d8583e34d731e8640cd5e8e076b260f41b094f
99cone <qnfrksmd0@gmail.com>
2021-12-05 15:42:22 +0900

    WIP

--- src/pages/Product_list/ProductList.js
+++ src/pages/Product_list/ProductList.js
@@ -1,6 +1,34 @@
 import React, { useState } from 'react';
 import './ProductList.scss';
 
+// function ProductList() {
+//   return (
+//     <div className="galleylist">
+//       <ul>
+//         <li>
+//           <a href="">
+//             <div className="screen">
+//               <div className="top">벨벳 크리오스 이불세트</div>
+//               <div className="botton">
+//                 <div className="bottonlist">
+//                   <span>장바구니에 담기</span>
+//                 </div>
+//               </div>
+//               <img src="img/velvet.jpg" alt="velvet" />
+//             </div>
+//             <div>
+//               <h3>벨벳 크리오스 이불세트</h3>
+//               <h3>119,000 원</h3>
+//             </div>
+//           </a>
+//         </li>
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default ProductList;
+
 function ProductList() {
   return (
     <div className="galleylist">
@@ -8,14 +36,17 @@ function ProductList() {
         <li>
           <a href="">
             <div className="screen">
-              <div className="top">자라나라</div>
+              <div className="top">벨벳 크리오스 이불세트</div>
               <div className="botton">
-                <span>장바구니에 담기</span>
+                <div className="bottonlist">
+                  <span>장바구니에 담기</span>
+                </div>
               </div>
               <img src="img/velvet.jpg" alt="velvet" />
             </div>
-            <div>
+            <div className="productinfo">
               <h3>벨벳 크리오스 이불세트</h3>
+              <h3>119,000 원</h3>
             </div>
           </a>
         </li>
