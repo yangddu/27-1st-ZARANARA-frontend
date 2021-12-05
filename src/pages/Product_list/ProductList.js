7dfc46387f3e0c3e9a2e864139c3e5749101e220
99cone <qnfrksmd0@gmail.com>
2021-12-05 16:47:10 +0900

    WIP

--- src/pages/Product_list/ProductList.js
+++ src/pages/Product_list/ProductList.js
@@ -51,6 +51,25 @@ function ProductList() {
           </a>
         </li>
       </ul>
+      <ul>
+        <li>
+          <a href="">
+            <div className="screen">
+              <div className="top">벨벳 크리오스 이불세트</div>
+              <div className="botton">
+                <div className="bottonlist">
+                  <span>장바구니에 담기</span>
+                </div>
+              </div>
+              <img src="img/velvet.jpg" alt="velvet" />
+            </div>
+            <div className="productinfo">
+              <h3>벨벳 크리오스 이불세트</h3>
+              <h3>119,000 원</h3>
+            </div>
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
