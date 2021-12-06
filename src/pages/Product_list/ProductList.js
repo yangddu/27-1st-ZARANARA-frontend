b8264698651f0479448f243955df105e8311e99e
99cone <qnfrksmd0@gmail.com>
2021-12-06 10:54:03 +0900

    WIP

--- src/pages/Product_list/ProductList.js
+++ src/pages/Product_list/ProductList.js
@@ -3,27 +3,33 @@ import './ProductList.scss';
 
 // function ProductList() {
 //   return (
-//     <div className="galleylist">
-//       <ul>
-//         <li>
-//           <a href="">
-//             <div className="screen">
-//               <div className="top">벨벳 크리오스 이불세트</div>
-//               <div className="botton">
-//                 <div className="bottonlist">
-//                   <span>장바구니에 담기</span>
+//     <>
+//       <div className="galleylist">
+//         <ul>
+//           <li>
+//             <a href="">
+//               <div className="screen">
+//                 {/* <div className="top">벨벳 크리오스 이불세트</div> */}
+//                 <div className="botton">
+//                   <div className="bottonlist">
+//                     <span>장바구니에 담기</span>
+//                   </div>
+//                 </div>
+//                 <img src="img/velvet.jpg" alt="velvet" />
+//               </div>
+//               <div className="productinfo">
+//                 <div className="productName">
+//                   <h3>상품명</h3>
+//                 </div>
+//                 <div className="productPrice">
+//                   <h3>가격정보</h3>
 //                 </div>
 //               </div>
-//               <img src="img/velvet.jpg" alt="velvet" />
-//             </div>
-//             <div>
-//               <h3>벨벳 크리오스 이불세트</h3>
-//               <h3>119,000 원</h3>
-//             </div>
-//           </a>
-//         </li>
-//       </ul>
-//     </div>
+//             </a>
+//           </li>
+//         </ul>
+//       </div>
+//     </>
 //   );
 // }
 
@@ -31,46 +37,33 @@ import './ProductList.scss';
 
 function ProductList() {
   return (
-    <div className="galleylist">
-      <ul>
-        <li>
-          <a href="">
-            <div className="screen">
-              <div className="top">벨벳 크리오스 이불세트</div>
-              <div className="botton">
-                <div className="bottonlist">
-                  <span>장바구니에 담기</span>
+    <>
+      <div className="galleylist">
+        <ul>
+          <li>
+            <a href="">
+              <div className="screen">
+                {/* <div className="top">벨벳 크리오스 이불세트</div> */}
+                <div className="botton">
+                  <div className="bottonlist">
+                    <span>장바구니에 담기</span>
+                  </div>
+                </div>
+                <img src="img/velvet.jpg" alt="velvet" />
+              </div>
+              <div className="productinfo">
+                <div className="productName">
+                  <h3>상품명</h3>
+                </div>
+                <div className="productPrice">
+                  <h3>가격정보</h3>
                 </div>
               </div>
-              <img src="img/velvet.jpg" alt="velvet" />
-            </div>
-            <div className="productinfo">
-              <h3>벨벳 크리오스 이불세트</h3>
-              <h3>119,000 원</h3>
-            </div>
-          </a>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <a href="">
-            <div className="screen">
-              <div className="top">벨벳 크리오스 이불세트</div>
-              <div className="botton">
-                <div className="bottonlist">
-                  <span>장바구니에 담기</span>
-                </div>
-              </div>
-              <img src="img/velvet.jpg" alt="velvet" />
-            </div>
-            <div className="productinfo">
-              <h3>벨벳 크리오스 이불세트</h3>
-              <h3>119,000 원</h3>
-            </div>
-          </a>
-        </li>
-      </ul>
-    </div>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 }
 
