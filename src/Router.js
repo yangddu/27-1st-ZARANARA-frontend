9d13bcb171efc15f87cb4d932dfb3439d9e65827
99cone <qnfrksmd0@gmail.com>
2021-12-06 18:04:25 +0900

    WIP

--- src/Router.js
+++ src/Router.js
@@ -5,18 +5,18 @@ import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
 import Login from './pages/Login/Login';
 import JoinModal from './pages/JoinModal/JoinModal';
-import ProductList from './pages/Product_list/ProductList';
+import ProductList from './pages/Product_list/productList/ProductList';
 
 const Router = () => (
   <BrowserRouter>
-    {/* <Nav /> */}
+    <Nav />
     <Routes>
       <Route path="/product" element={<ProductList />} />
       <Route path="/join" element={<JoinModal />} />
       <Route path="/login" element={<Login />} />
       <Route path="/" element={<Main />} />
     </Routes>
-    {/* <Footer /> */}
+    <Footer />
   </BrowserRouter>
 );
 
