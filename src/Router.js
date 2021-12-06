dbb1f36c290baca0b80b91604cac9432c116350b
sozeroJung <sozero.jung@gmail.com>
2021-12-06 21:32:51 +0900

    Fix: Nav 컴포넌트 레이아웃 수정

--- src/Router.js
+++ src/Router.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
 import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
+import Search from './pages/Search/Search';
 
 const Router = () => (
   <BrowserRouter>
     <Nav />
     <Routes>
       <Route path="/" element={<Main />} />
+      <Route path="/search" element={<Search />} />
     </Routes>
     <Footer />
   </BrowserRouter>
