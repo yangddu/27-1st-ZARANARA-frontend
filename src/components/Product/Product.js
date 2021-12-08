b7f5679447db70189aed543c885766d6a866cef8
sozeroJung <sozero.jung@gmail.com>
2021-12-09 08:06:37 +0900

    Fix: 메인페이지 제품 리스트 spot 위치 수정

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -7,18 +7,18 @@ const Product = () => {
   const [productImages, setProductImages] = useState([]);
 
   // 백엔드 데이터 통신용
-  useEffect(() => {
-    fetch(`${API.PRODUCT}/set\?offset\=0\&limit\=2`)
-      .then(res => res.json())
-      .then(data => setProductImages(data.list_item));
-  }, []);
+  // useEffect(() => {
+  //   fetch(`${API.PRODUCT}/set\?offset\=0\&limit\=2`)
+  //     .then(res => res.json())
+  //     .then(data => setProductImages(data.list_item));
+  // }, []);
 
   // mock 데이터 통신용
-  // useEffect(() => {
-  //   fetch('/data/productMockData.json')
-  //     .then(res => res.json())
-  //     .then(data => setProductImages(data));
-  // }, []);
+  useEffect(() => {
+    fetch('/data/productMockData.json')
+      .then(res => res.json())
+      .then(data => setProductImages(data));
+  }, []);
 
   return (
     <section className="productContainer">
