0a9850855ed4f717774d5b9861601d8d07e0ffe8
sozeroJung <sozero.jung@gmail.com>
2021-12-08 21:27:56 +0900

    Fix: 제품 리스트 페이지 레이아웃 수정

--- src/pages/Product_list/FilterAside/FilterAside.js
+++ src/pages/Product_list/FilterAside/FilterAside.js
@@ -4,44 +4,37 @@ import './FilterAside.scss';
 
 function FilterAside({
   isFilterOn,
-  handleFilter,
+  handleOpenFilterAside,
   handleFilterLowerPrice,
   handleFilterTopPrice,
 }) {
   return (
-    <aside className={isFilterOn ? 'filterAside active' : 'filterAside'}>
+    <aside className={`filterAside ${isFilterOn ? 'active' : ''}`}>
       <div className="header">
         <div className="titleWrapper">
           <span className="titlename">필터</span>
+          <button
+            className="closeButton"
+            onClick={() => handleOpenFilterAside()}
+          >
+            <IoCloseSharp className="closeIcon" />
+          </button>
         </div>
-        <button className="closeButton" onClick={() => handleFilter()}>
-          {/* <IoCloseSharp className="close" /> */}
-        </button>
-        <form className="buttonContainer">
-          <div>
-            <input
-              onClick={handleFilterTopPrice}
-              type="radio"
-              id="topPrice"
-              name="priceFilter"
-              value="topPrice"
-              checked
-            />
-            <label htmlFor="huey">높은 순</label>
-          </div>
-          <div>
-            <input
-              onClick={handleFilterLowerPrice}
-              type="radio"
-              id="lowerPrice"
-              name="priceFilter"
-              value="lowerPrice"
-            />
-            <label htmlFor="dewey">낮은 순</label>
-          </div>
-        </form>
       </div>
-      <div className="bottom" />
+      <div className="buttonContainer">
+        <button
+          className="topPriceButton"
+          onClick={() => handleFilterTopPrice()}
+        >
+          높은 순
+        </button>
+        <button
+          className="lowerPriceButton"
+          onClick={() => handleFilterLowerPrice()}
+        >
+          낮은 순
+        </button>
+      </div>
     </aside>
   );
 }
