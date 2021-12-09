2b25e1d442101ed20cdbfbd8e6c945ee8665ebc7
sozeroJung <sozero.jung@gmail.com>
2021-12-09 09:23:52 +0900

    Add: Nav 컴포넌트 장바구니아이콘 호버 기능 추가

--- src/components/Cart/LaterShop/LaterShop.js
+++ src/components/Cart/LaterShop/LaterShop.js
@@ -2,9 +2,8 @@ import React from 'react';
 
 import './LaterShop.scss';
 
-const EMPTY_LATER_SHOPPING_MENT =
-  '나중에 쇼핑하기 위해 저장한 상품 목록이 비어 있습니다.';
-
-const LaterShop = () => <p className="ment">{EMPTY_LATER_SHOPPING_MENT}</p>;
+const LaterShop = () => (
+  <p className="ment">나중에 쇼핑하기 위해 저장한 상품 목록이 비어 있습니다.</p>
+);
 
 export default LaterShop;
--- src/components/Cart/Price/Price.js
+++ src/components/Cart/Price/Price.js
@@ -2,9 +2,6 @@ import React from 'react';
 
 import './Price.scss';
 
-const PROMOTION_MENT =
-  '프로모션 코드가 있으신가요? 나중에 결제 페이지에서 입력하십시오.';
-
 const Price = ({ totalCount, totalPrice }) => (
   <article className="cartContentRight">
     <span className="totalCount">{totalCount} 제품</span>
@@ -12,7 +9,9 @@ const Price = ({ totalCount, totalPrice }) => (
       <span className="totalText">합계:</span>
       <span className="totalPrice">{totalPrice.toLocaleString()} 원</span>
     </div>
-    <p className="promotionInfo">{PROMOTION_MENT}</p>
+    <p className="promotionInfo">
+      프로모션 코드가 있으신가요? 나중에 결제 페이지에서 입력하십시오.
+    </p>
     <button className="orderButton">주문하기</button>
   </article>
 );
